8ebe03816d479c3bef9002fe6a98d5fa3a024250
JackieBowers19 <56894731+JackieBowers19@users.noreply.github.com>
2019-10-29 16:11:51 -0500

    only parse cap specific structure

--- CapParser.cpp
+++ CapParser.cpp
@@ -6,22 +6,37 @@ using namespace rapidxml;
 using namespace std;
 
 CapParser::CapParser(char* filename){
-	file xmlFile(filename);
-	//file xmlFile("/Users/jackie/test.xml");
-    doc.parse<0>(xmlFile.data());
+	cout << "parse xml";
+	ifstream theFile (filename);
+	vector<char> buffer((istreambuf_iterator<char>(theFile)), istreambuf_iterator<char>());
+	buffer.push_back('\0');
+	doc.parse<0>(&buffer[0]);
 }
 
 void CapParser::getCapNodes() {
     xml_node<> *cap_node = doc.first_node();
-    xml_attribute<>* alert_version;
-    xml_node<> *first_node = doc.first_node();
-    if (first_node->name() == "cap:alert") {
-        alert_version = first_node->first_attribute();
+    string alert_version;
+    if (strncmp(cap_node->name(), "cap:alert", 100) == 0) {
+        alert_version = cap_node->first_attribute()->value();
+        cout << alert_version;
     }
     // else invalid format, CAP version must be specified
 
-    // For now, get all relevant CAP attributes in any order until we are certain of xml structure. 
-    vector<vector<string> > cap_vector;
+    this->identifier = cap_node->first_node("cap:identifier")->value();
+    this->sender = cap_node->first_node("cap:sender")->value();
+    this->status = cap_node->first_node("cap:status")->value();
+    this->type = cap_node->first_node("cap:msgType")->value();
+
+    xml_node<> *info_node = cap_node->first_node("cap:info");
+
+    this->event = info_node->first_node("cap:event")->value();
+    this->severity = info_node->first_node("cap:severity")->value();
+    this->certainty = info_node->first_node("cap:certainty")->value();
+    this->description = info_node->first_node("cap:description")->value();
+    this->area = info_node->first_node("cap:area")->value();
+
+
+    /*vector<vector<string> > cap_vector;
     capParse(first_node, cap_vector);
 
     for (auto &cap_iterator : cap_vector) {
@@ -34,7 +49,7 @@ void CapParser::getCapNodes() {
         else if (cap_iterator[0] == "certainty") certainty = cap_iterator[1];
         else if (cap_iterator[0] == "headline") headline = cap_iterator[1];
         else if (cap_iterator[0] == "description") description = cap_iterator[1];
-    }
+    }*/
 }
 
 void CapParser::capParse(xml_node<>* cap_node, vector<vector<string> > &all_node_vector) {
