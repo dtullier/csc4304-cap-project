e3aea3355cd9a875a3b56e2d2b5d52021a4d42ba
JackieBowers19 <56894731+JackieBowers19@users.noreply.github.com>
2019-10-29 13:08:02 -0500

    fixed errors

--- CapParser.hpp
+++ CapParser.hpp
@@ -8,16 +8,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "rapidxml-1.13/rapidxml.hpp"
+#include "rapidxml.hpp"
+#include "rapidxml_utils.hpp"
+
+using namespace rapidxml;
+using namespace std;
 
 class CapParser
 {
     private:
-        string sender, status, type, event, severity, certainty, headline, description
+        string sender, status, type, event, severity, certainty, headline, description;
         xml_document<> doc;
-        capParse(xml_node<>* cap_node, vector<vector<string>> &node_vector)
+        void capParse(xml_node<>* cap_node, vector<vector<string> > &node_vector);
     public:
-        CapParser(string filename);
-        getCapNodes();
-}
+        CapParser(char* filename);
+        void getCapNodes();
+};
 #endif
