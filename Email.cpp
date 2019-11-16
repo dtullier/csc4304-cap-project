e58ca709d181a3b38485bae32ec3b06292675b8f
David Burns <davidburns612@gmail.com>
2019-11-15 18:19:25 -0600

    Update Email.cpp

--- Email.cpp
+++ Email.cpp
@@ -1,56 +1,128 @@
-//author: Mark M.
-//using EASendMail
+/*
+ * SMTP example showing how to send e-mails
+ 
+ */ 
+ // sudo apt-get install libcurl4-gnutls-dev
+ 
+#include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
+ 
+/* This is a simple example showing how to send mail using libcurl's SMTP
+ * capabilities. For an example of using the multi interface please see
+ * smtp-multi.c.
+ *
+ * Note that this example requires libcurl 7.20.0 or above.
+ */ 
+ 
+#define FROM    "watchdogsnoreply@gmail.com"
+#define TO		"danton6@lsu.edu"
+#define CC		"davidburns612@gmail.com"
 
-#include <iostream>
-#include <tchar.h>
-#include <Windows.h>
-#include "easendmailobj.tlh"
-	using namespace EASendMailObjLib;
+ 
+static const char *payload_text[] = {
+  "Date: Mon, 29 Nov 2010 21:54:29 +1100\r\n",
+  "To: " TO "\r\n",
+  "From: " FROM "(Example User)\r\n",
+  "Cc: " CC "(Another example User)\r\n",
+  "Subject: SMTP example message\r\n",
+  "\r\n", /* empty line to divide headers from body, see RFC5322 */ 
+  "The body of the message starts here.\r\n",
+  "\r\n",
+  "It could be a lot of lines, could be MIME encoded, whatever.\r\n",
+  "Check RFC5322.\r\n",
+  NULL
+};
+ 
+struct upload_status {
+  int lines_read;
+};
+ 
+static size_t payload_source(void *ptr, size_t size, size_t nmemb, void *userp)
+{
+  struct upload_status *upload_ctx = (struct upload_status *)userp;
+  const char *data;
+ 
+  if((size == 0) || (nmemb == 0) || ((size*nmemb) < 1)) {
+    return 0;
+  }
+ 
+  data = payload_text[upload_ctx->lines_read];
+ 
+  if(data) {
+    size_t len = strlen(data);
+    memcpy(ptr, data, len);
+    upload_ctx->lines_read++;
+ 
+    return len;
+  }
+ 
+  return 0;
+}
+ 
+int main(void)
+{
+  CURL *curl;
+  CURLcode res = CURLE_OK;
+  struct curl_slist *recipients = NULL;
+  struct upload_status upload_ctx;
+ 
+  upload_ctx.lines_read = 0;
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    /* This is the URL for your mailserver */ 
+    curl_easy_setopt(curl, CURLOPT_URL, "smtp://smtp.gmail.com:587");
 
-	const int ConnectNormal = 0;
-	const int ConnectSSLAuto = 1;
-	const int ConnectSTARTTLS = 2;
-	const int ConnectDirectSSL = 3;
-	const int ConnectTryTLS = 4;
+    /* Note that this option isn't strictly required, omitting it will result
+     * in libcurl sending the MAIL FROM command with empty sender data. All
+     * autoresponses should have an empty reverse-path, and should be directed
+     * to the address in the reverse-path which triggered them. Otherwise,
+     * they could cause an endless loop. See RFC 5321 Section 4.5.5 for more
+     * details.
+     */ 
+    curl_easy_setopt(curl, CURLOPT_MAIL_FROM, FROM);
+ 
+    /* Add two recipients, in this particular case they correspond to the
+     * To: and Cc: addressees in the header, but they could be any kind of
+     * recipient. */ 
+    recipients = curl_slist_append(recipients, TO);
+    recipients = curl_slist_append(recipients, CC);
+    curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+ 
+    /* We're using a callback function to specify the payload (the headers and
+     * body of the message). You could just use the CURLOPT_READDATA option to
+     * specify a FILE pointer to read from. */ 
+    curl_easy_setopt(curl, CURLOPT_READFUNCTION, payload_source);
+    curl_easy_setopt(curl, CURLOPT_READDATA, &upload_ctx);
+    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+ 
+	curl_easy_setopt(curl, CURLOPT_USERNAME, "watchdogsnoreply@gmail.com");
+    curl_easy_setopt(curl, CURLOPT_PASSWORD, "watchdogs123");
 
-	int _tmain(int argc, _TCHAR * argv[])
-	{
-		::CoInitialize(NULL);
-
-		IMailPtr oSmtp = NULL;
-		oSmtp.CreateInstance(__uuidof(EASendMailObjLib::Mail));
-
-		oSmtp->LicenseCode = _T("TryIt");
-
-		oSmtp->FromAddr = _T("watchdogsnoreply@gmail.com");
-
-		oSmtp->AddRecipientEx(_T("remarkablymire@gmail.com"), 0);
-
-        oSmtp->Subject = _T("Watchdogs Test");
-
-		oSmtp->BodyText = _T("Watchdogs Test");
-
-		oSmtp->ServerAddr = _T("smtp.gmail.com");
-
-		oSmtp->UserName = _T("watchdogsnoreply@gmail.com");
-
-		oSmtp->Password = _T("watchdogs123");
-
-		oSmtp->ConnectType = ConnectTryTLS;
-		oSmtp->ServerPort = 587; // 25 or 587 or 465
-		oSmtp->ConnectType = ConnectSSLAuto;
-
-		_tprintf(_T("Attemping to send email ...\r\n"));
-
-		if (oSmtp->SendMail() == 0)
-		{
-			_tprintf(_T("Email was sent successfully!\r\n"));
-		}
-		else
-		{
-			_tprintf(_T("Failed to send email with the following error: %s\r\n"),
-				(const TCHAR*)oSmtp->GetLastErrDescription());
-		}
-
-		return 0;
-	}
+	curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
+ 
+    /* Send the message */ 
+    res = curl_easy_perform(curl);
+ 
+    /* Check for errors */ 
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* Free the list of recipients */ 
+    curl_slist_free_all(recipients);
+ 
+    /* curl won't send the QUIT command until you call cleanup, so you should
+     * be able to re-use this connection for additional messages (setting
+     * CURLOPT_MAIL_FROM and CURLOPT_MAIL_RCPT as required, and calling
+     * curl_easy_perform() again. It may not be a good idea to keep the
+     * connection open for a very long time though (more than a few minutes
+     * may result in the server timing out the connection), and you do want to
+     * clean up in the end.
+     */ 
+    curl_easy_cleanup(curl);
+  }
+ 
+  return (int)res;
+}
