cc1f6c21e66e77f7e5a18784207eb1f075d9b3c8
remarkablymark <remarkablymire@gmail.com>
2019-11-17 15:12:42 -0600

    Update Email.cpp

--- Email.cpp
+++ Email.cpp
@@ -1,20 +1,13 @@
-/*
- * SMTP example showing how to send e-mails
- 
- */ 
+ /*  adapted from: https://www.phidgets.com/?view=articles&article=LIBCURLHowTo
  // sudo apt-get install libcurl4-gnutls-dev
  
+ * Requires libcurl 7.20.0 or above.
+ */ 
+
 #include <stdio.h>
 #include <string.h>
 #include <curl/curl.h>
- 
-/* This is a simple example showing how to send mail using libcurl's SMTP
- * capabilities. For an example of using the multi interface please see
- * smtp-multi.c.
- *
- * Note that this example requires libcurl 7.20.0 or above.
- */ 
- 
+
 #define FROM    "watchdogsnoreply@gmail.com"
 #define TO		"danton6@lsu.edu"
 #define CC		"davidburns612@gmail.com"
@@ -71,33 +64,24 @@ int main(void)
  
   curl = curl_easy_init();
   if(curl) {
-    /* This is the URL for your mailserver */ 
     curl_easy_setopt(curl, CURLOPT_URL, "smtp://smtp.gmail.com:587");
-
-    /* Note that this option isn't strictly required, omitting it will result
-     * in libcurl sending the MAIL FROM command with empty sender data. All
-     * autoresponses should have an empty reverse-path, and should be directed
-     * to the address in the reverse-path which triggered them. Otherwise,
-     * they could cause an endless loop. See RFC 5321 Section 4.5.5 for more
-     * details.
-     */ 
+	  
     curl_easy_setopt(curl, CURLOPT_MAIL_FROM, FROM);
- 
-    /* Add two recipients, in this particular case they correspond to the
-     * To: and Cc: addressees in the header, but they could be any kind of
-     * recipient. */ 
+	  
     recipients = curl_slist_append(recipients, TO);
+	  
     recipients = curl_slist_append(recipients, CC);
+	  
     curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
  
-    /* We're using a callback function to specify the payload (the headers and
-     * body of the message). You could just use the CURLOPT_READDATA option to
-     * specify a FILE pointer to read from. */ 
+   
     curl_easy_setopt(curl, CURLOPT_READFUNCTION, payload_source);
+	  
     curl_easy_setopt(curl, CURLOPT_READDATA, &upload_ctx);
+	  
     curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
  
-	curl_easy_setopt(curl, CURLOPT_USERNAME, "watchdogsnoreply@gmail.com");
+    curl_easy_setopt(curl, CURLOPT_USERNAME, "watchdogsnoreply@gmail.com");
     curl_easy_setopt(curl, CURLOPT_PASSWORD, "watchdogs123");
 
 	curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
@@ -113,14 +97,7 @@ int main(void)
     /* Free the list of recipients */ 
     curl_slist_free_all(recipients);
  
-    /* curl won't send the QUIT command until you call cleanup, so you should
-     * be able to re-use this connection for additional messages (setting
-     * CURLOPT_MAIL_FROM and CURLOPT_MAIL_RCPT as required, and calling
-     * curl_easy_perform() again. It may not be a good idea to keep the
-     * connection open for a very long time though (more than a few minutes
-     * may result in the server timing out the connection), and you do want to
-     * clean up in the end.
-     */ 
+    
     curl_easy_cleanup(curl);
   }
  
